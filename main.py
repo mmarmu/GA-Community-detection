98a10c3bd2906e7512aca02a66145f666d8c0b79
marmu123 <marmureanumarius@yahoo.com>
2020-03-23 23:41:43 +0200

    bug fixed(community 0)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ui.ui import UI
 from service.Service import Service
 from repository.Repository import Repository
-repo=Repository("data/krebs.gml")
+repo=Repository("data/polbooks.gml")
 serv=Service(repo)
 ui=UI(serv)
 ui.run()
